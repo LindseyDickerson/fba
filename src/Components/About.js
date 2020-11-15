3445370632e4917f0aca1457324f04c55bda8fe8
Lindsey Dickerson <lindseydickerson2@gmail.com>
2020-11-15 02:00:51 -0500

    faq and about content added

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,7 +5,19 @@ function About() {
     return (
       <div className="About">
 
-      About content goes here
+Looking back now it’s easy to see how I went down the path of baking bread, cooking in general and ended up teaching these bread-baking classes.  We grew up in the Chicago suburb of Chicago Heights, IL. Our dad’s family was Italian and my mom’s family was from Southwest Arkansas so I had the best of both worlds when it came to food.  I watched every classic cooking show you could think of growing up.  
+<br/>
+My Italian grandmother was a great baker and her recipe is featured in my Italian bread class. My grandmother from Malvern, AR fixed cornbread, biscuits & peach cobbler when we visited those hot summers.   
+<br/>
+My dad also worked for (go figure) Wonder Bread in Matteson, IL for many years. He did everything from drive the bread trucks to manage the drivers and routes. I remember going in with him on weekends while he did paperwork. I’d sit in the trucks, honk the horns and be all by myself in a warehouse full of bread. While it may be only be wonder Bread the smell was pretty memorable. I never ate another Twinkie, Ho-Ho or Ding-Dong since.  
+<br/>
+After leaving Chicago Heights, IL for West Lafayette, IN to enter the real world I craved the breads & pizzas I could not get outside my hometown. We received a pizza stone as a wedding gift and I started experimenting with pizzas which led to bread-baking. I’ve done my share of county & state fair entries- have a few ribbons too. I experimented with many different breads and usually went with what my family liked to eat.  
+<br/>
+The Great Fermentations Connection: 
+<br/>
+One of my other hobbies was homebrewing and judging beers at competitions. As part of our home brewer’s club we had gatherings and I always brought bread (and still do). It’s where I picked up the nickname ‘Frank the Bread Guy”. The owner of Great Fermentations Anita Johnson was just getting their new location set up and she would always ask me to teach bread classes. At the time I was a Field Service Engineer with demanding hours and had to pass. Once I went into management we sat down and put together a couple basic classes. The folks at great Fermentations are awesome- Jenny Kraner schedules and promotes the classes so they show up on Event Brite. I just show up to teach the class.  
+<br/>
+Now in my 4th season of teaching these classes they have exceeded my expectations on every level. People truly are curious and passionate about making bread. I have had several folks attend every class I teach. I see Mother/daughter, couples, groups and some even drive from Illinois or Ohio to attend. There is no greater honor. 
       </div>
     );
   }
